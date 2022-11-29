83d6b4a6888104cbcb761027ac2f19742d3e0f28
Vinicius Campos <vinicamposdev@gmail.com>
2022-11-29 20:40:46 -0300

    build: :building_construction: add secrets

--- server.go
+++ server.go
@@ -10,12 +10,19 @@ import (
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("SERVER_NAME")
 	fmt.Fprintf(w, "<h1>Hello World, from %s</h1>", name)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "<h1>Hello World, from %s</h1>", password)
 }
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {

df1823b86104d75ba575e329f4f2523bedc41103
Vinicius Campos <vinicamposdev@gmail.com>
2022-11-30 06:42:54 -0300

    feat: :sparkles: prepare healthz to get more easier to make stress tests

--- server.go
+++ server.go
@@ -38,12 +38,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
 
-	if duration.Seconds() > 25 {
+	if duration.Seconds() < 10 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
 	} else {
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("ok")))
+		w.Write([]byte("ok"))
 	}
-
 }

d1c5a607bf8e18f049a8828b5b2b249c724ffad8
Vinicius Campos <vinicamposdev@gmail.com>
2022-12-09 19:00:41 -0300

    feat: :sparkles: add kafka consumer

--- kafka/cmd/consumer/main.go
+++ kafka/cmd/consumer/main.go
@@ -1,1 +1,27 @@
-package consumer
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka_kafka_1:9092",
+		"client.id":         "goapp-consumer",
+		"group.id":          "goapp-group",
+		"auto.offset.reset": "earliest",
+	}
+	c, err := kafka.NewConsumer(configMap)
+	if err != nil {
+		fmt.Println("error consumer", err.Error())
+	}
+	topics := []string{"test"}
+	c.SubscribeTopics(topics, nil)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Println(string(msg.Value), msg.TopicPartition)
+		}
+	}
+}

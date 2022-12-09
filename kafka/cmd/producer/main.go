5921bacee5dc0bdf9b2c64ddcab342d7cf89918e
Vinicius Campos <vinicamposdev@gmail.com>
2022-12-09 12:10:48 -0300

    feat: :sparkles: ensure message order sent keys

--- kafka/cmd/producer/main.go
+++ kafka/cmd/producer/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("message", "teste", producer, nil)
+	Publish("message", "test", producer, []byte("transference"), deliveryChan)
 	go DeliveryReport(deliveryChan) // async
 }
 
 func NewKafkaProducer() *kafka.Producer {
-	configMap := kafka.ConfigMap{
-		"bootstrap.servers":   "kafka-kafka-1:9092",
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers":   "kafka_kafka_1:9092",
 		"delivery.timeout.ms": "0",
 		"ack":                 "all",
 		"enable.idempotence":  "true",
@@ -28,26 +28,25 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	return err
 }
 
 func DeliveryReport(deliveryChan chan kafka.Event) {
-	for e := e.(type) {
+	for e := range deliveryChan {
 		switch ev := e.(type) {
 		case *kafka.Message:
 			if ev.TopicPartition.Error != nil {
-				fmt.Println("Error on sent")
+				fmt.Println("Error on sent message")
 			} else {
-				fmt.Println("Message sent", ev.TopicPartition)
+				fmt.Println("Message sent:", ev.TopicPartition)
 			}
-
 		}
 	}
 }

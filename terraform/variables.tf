f0aad8e12012ab8989dc5bdfb85ddcbde82ef913
Vinicius Campos <vinicamposdev@gmail.com>
2022-12-10 18:42:20 -0300

    build(terraform): :wrench: add modules to config cluster eks

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+
+variable "prefix" {}
+variable "vpc_cidr_block" {}
+variable "cluster_name" {}
+variable retention_days {}
+variable "desired_size" {}
+variable "max_size" {}
+variable "min_size" {}
